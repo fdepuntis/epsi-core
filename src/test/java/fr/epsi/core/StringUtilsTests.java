cd5de6b6074a09041eec0cebf36c15bb3742164f
unknown <fdepuntis@btslivh.eu>
2021-11-18 16:37:11 +0100

    version 3 avec palindrome

--- src/test/java/fr/epsi/core/StringUtilsTests.java
+++ src/test/java/fr/epsi/core/StringUtilsTests.java
@@ -1,9 +1,11 @@
 package fr.epsi.core;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class StringUtilsTests {
@@ -13,12 +15,33 @@ class StringUtilsTests {
 
 	}
 
-	@ParameterizedTest
-	@ValueSource(strings = { "ete", "a but tuba" }) // Le test sera lancé 6 fois
+	@ParameterizedTest(name = "{0} est un palindrome")
+	@ValueSource(strings = { "ete", "a but tuba" })
 
 	void isPalindrome(String sentense) {
 		assertTrue(StringUtils.isPalindrome(sentense));
 
 	}
 
+	@ParameterizedTest(name = "{0} n est pas un palindrome")
+	@ValueSource(strings = { "ile", "je code" })
+
+	void isNotPalindrome(String sentense) {
+		assertFalse(StringUtils.isPalindrome(sentense));
+
+	}
+
+	@ParameterizedTest(name = "{0} ")
+	@CsvFileSource(resources = "/cesar.csv")
+
+	void cesarEncode(String sentence, int step) {
+
+	}
+
+	@ParameterizedTest(name = "{0} ")
+	@CsvFileSource(resources = "/cesar.csv")
+
+	void cesarDecode(String sentence, int step) {
+
+	}
 }

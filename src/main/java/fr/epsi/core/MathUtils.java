bd2e5b608e3a0f5ac529e3b891983264424ce321
fdepuntis <77015977+fdepuntis@users.noreply.github.com>
2022-01-24 16:00:46 +0100

    Update MathUtils.java

--- src/main/java/fr/epsi/core/MathUtils.java
+++ src/main/java/fr/epsi/core/MathUtils.java
@@ -6,7 +6,7 @@ public final class MathUtils {
 
 
     /**
-     * Calculates the factorial of the number n recursively. Ajout du texte essai
+     * Calculates the factorial of the number n recursively. Ajout du texte essai2
      *
      * @param n the number n.
      * @return the factorial.
